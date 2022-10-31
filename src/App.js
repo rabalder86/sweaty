f15849b907dfc79a134b89305887eb7f01b4a28b
Nathi Geigant <98586239+rabalder86@users.noreply.github.com>
2022-10-31 11:28:32 +0100

    For pull req

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload for helvete
+          Edit <code>src/App.js</code> and save to reload for helvetehejhejgit
         </p>
         <a
           className="App-link"
